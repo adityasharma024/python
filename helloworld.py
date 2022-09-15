2c41060082fb8054a1535a7bca632ed03e4751a4
ADITYA SHARMA <91861881+adityasharma024@users.noreply.github.com>
2022-09-15 21:23:05 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+from calendar import c
+import sys
+print('hello world')
+print("Hello world")
+print('''Hey! I'm "best"''')
+s='2A section '
+p='python'
+print(s+p)
+print(s+'hello')
+guest='hari'
+room=502
+print(guest,'got room no',room) #type casting is not allowed#
+print(5.0/2.0)
+print(5.0/2)
+print(5//2)
+print(5/2)
+print(sys.getsizeof((5)))
+print(sys.getsizeof(s))
+print(sys.getsizeof('c'))
+print(sys.getsizeof('hii'))
+print(sys.getsizeof(0))
+
+
